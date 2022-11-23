70618241352884239355721261f2cf7c35144f68
abdelrhman04 <abdomajde04@gmail.com>
2022-11-23 16:09:05 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7]{ 1,2,3,4,5,6,7}; 
+            Console.WriteLine("Enter Number ");
+            int Number =int.Parse(Console.ReadLine());
+           int Status= linearseacher(arr, Number);
+            if(Status!= -1)
+            {
+                Console.WriteLine("Find "+ Number + " In Index : "+ Status);
+            }
+            else
+            {
+                Console.WriteLine("Not Found This Number");            }
+        }
+        public static int linearseacher(int[] number ,int n)
+        {
+            int NotFound = -1;
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index]==n)
+                {
+                    return index;
+
+                }
+
+            }
+            return NotFound;
+        }
+
+    }
+}
